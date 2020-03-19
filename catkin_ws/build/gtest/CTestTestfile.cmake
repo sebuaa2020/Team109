e7a50679e4edcda13f3a2069387d41433892093d
qushuo <m18810960119@163.com>
2020-03-19 18:52:39 +0800

    init workspace

---
+++ catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/qushuo/work/term_2020_1/Team109/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
