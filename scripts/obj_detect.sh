6adf42140f931bf2c50a4669de48d65a1db7a531
qushuo <m18810960119@163.com>
2020-05-26 20:12:46 +0800

    init test and some scripts

--- scripts/obj_detect.sh
+++ scripts/obj_detect.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 # This script is to run object detection within ros
+
+# cd ros_ws
+CURR_DIR=$(dirname $(readlink -f $0))
+cd $CURR_DIR
+cd ../ros_ws
+
+# make ros
+catkin_make
+
+# setup environment
+source ./devel/setup.zsh
+
+# run object detection
+roslaunch wpr_simulation wpb_simple.launch
+roslaunch wpb_home_tutorials obj_detect.launch
+rosrun my_pkg obj_detect_recv_marker
--- setup.sh
+++ setup.sh
@@ -2,3 +2,23 @@
 # This script is to install ros and its dependencies
 
 
+if ! [ -d "/opt/ros/kinetic" ]; then
+  echo "install ros release version kinetic"
+
+  # install ros kinetic
+  sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+  sudo apt-get update
+  sudo apt-get install ros-kinetic-desktop-full
+
+  # environment setup
+  echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+  # install dependencies
+  sudo apt-get install python-rosdep python-rosinstall \
+    python-rosinstall-generator python-wstool build-essential
+  sudo apt-get install python-rosdep
+  sudo rosdep init
+  rosdep update
+fi
+
+echo "environment setup finish!"
