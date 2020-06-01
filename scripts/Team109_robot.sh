bc877b306a33bbbfef3d6bff5e3895ab26fb1900
qushuo <m18810960119@163.com>
2020-06-01 23:25:22 +0800

    add ui script

---
+++ scripts/Team109_robot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script is to run robot with gui
+
+
+# cd ros_ws
+CURR_DIR=$(dirname $(readlink -f $0))
+cd $CURR_DIR
+cd ../ros_ws
+
+# install dependencies
+#rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
+
+# make ros
+catkin_make
+
+# setup environment
+source ./devel/setup.bash
+
+# mkdir output dir
+cd $CURR_DIR # first cd script folder
+if ! [ -d main_controller ]; then
+  mkdir main_controller
+fi
+
+# launch
+roslaunch wpr_simulation wpb_simple.launch
+rosrun cyrobot_monitor cyrobot_monitor
